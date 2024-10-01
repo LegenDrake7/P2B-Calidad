e8e9cac9a0b1af451209cbddfa8797988abb7d48
Jose <josdelgar99@gmail.com>
2024-10-01 11:05:05 +0200

    Primera version empleado

---
+++ src/pkg/Empleado.java
@@ -0,0 +1,39 @@
+package pkg;
+
+public class Empleado {
+	
+	public enum TipoEmpleado{Vendedor, Encargado};
+	float salarioBase = 0;
+	float primas = 0;
+	float extras = 0;
+	
+	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		if(tipo==TipoEmpleado.Vendedor) {
+			salarioBase = 2000;
+		} else if(tipo==TipoEmpleado.Encargado) {
+			salarioBase = 2500;
+		}
+		
+		if(ventasMes>=1500) {
+			primas = 200;
+		} else if(ventasMes>=1000) {
+			primas = 100;
+		} 
+		
+		extras = horasExtra*30;
+		
+		return salarioBase + primas + extras;
+	}
+	
+	public float calculoNominaNeta(float nominaBruta) {
+		float retencion= 0;
+		
+		if(nominaBruta >= 2500) {
+			retencion = 0.18f;
+		} else if(nominaBruta >= 2100) {
+			retencion = 0.15f;
+		}
+		
+		return nominaBruta*(1-retencion);
+	}
+}
