2b34e557a7039e4733009ff724f8d5ac68232a15
Jose <josdelgar99@gmail.com>
2024-10-03 15:41:27 +0200

    Version Final

--- src/test/EmpleadoTest.java
+++ src/test/EmpleadoTest.java
@@ -17,18 +17,22 @@ class EmpleadoTest {
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		System.out.println("Before All");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		System.out.println("After All");
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		System.out.println("Before Each");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		System.out.println("After Each");
 	}
 
 	@Test
